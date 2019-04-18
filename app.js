07b30023296d3217eeb94b75db681b7619f1abf5
James H <jamesherrero@gmail.com>
2019-04-18 20:31:21 +1000

    working with basic serial communication

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ const parser = new Readline()
 port.pipe(parser)
 // parser.on('data', console.log)
 
-
 const path = require('path');
 const http = require('http');
 const express = require('express');
@@ -24,11 +23,9 @@ var server = http.createServer(app)
 var io = socketIO(server);
 app.use(express.static(publicPath));
 
-
 app.get('/', function (req, res) {
   res.sendFile(path.join(publicPath, 'index.html'))
 });
-
 
 port.on("open", () => {
   console.log("connection made")
@@ -41,10 +38,10 @@ const onData = (data) => {
 parser.on('data', onData);
 
 let on = () => {
-  port.write('hello');
+  port.write('1');
 }
 let off = () => {
-  // undefined
+  port.write('0');
 }
 
 io.on('connection', function (socket) {
@@ -52,21 +49,77 @@ io.on('connection', function (socket) {
 
   socket.on('on', function (data) {
     // console.log(data);
-    console.log('send h');
+    console.log('send 1');
     on();
   });
 
   socket.on('off', function (data) {
     // console.log(data);
-    console.log('do nothing');
-    // off();
+    console.log('send 0');
+    off();
   });
 });
-
-
-
 
 server.listen(3000, () => {
   console.log("Running on local host ");
 });
 
+// // When user selects a CandidateCard from home page
+// (<any>window).dataLayer.push({
+//   event: 'CandidateCardClick',
+//   candidate_id: '<add variable here>',
+//   candidate_code: '<add variable here>',
+//   consultant_id: '<add variable here>',
+//   candidate_status: '<add variable here>',
+//   order_id: 'not set',
+//   staticList_id: 'not set'
+// })
+
+
+// (<any>window).dataLayer.push({
+//   event: 'selectAllCandidate',
+//   candidate_id: '<add variable here>',
+//   candidate_code: '<add variable here>',
+//   consultant_id: '<add variable here>',
+//   candidate_status: '<add variable here>',
+//   order_id: 'not set',
+//   staticList_id: 'not set'
+// })
+
+// (<any>window).dataLayer.push({
+//   event: 'IndividualStaticListSelected',
+//   candidate_id: 'not set',
+//   candidate_code: 'not set',
+//   consultant_id: '<add varible>',
+//   candidate_status: 'not set,
+//   order_id: 'not set',
+//   staticList_id: 'not set',
+//   allSelectedCandidate: []
+// })
+
+// (<any>window).dataLayer.push({
+//   event: 'SendEmailClicked',
+//   candidate_id: 'not set',
+//   candidate_code: 'not set',
+//   consultant_id: '<add variable here>',
+//   candidate_status: 'not set',
+//   order_id: 'not set',
+//   staticList_id: 'not set'
+// })
+
+// (<any>window).dataLayer.push({
+//   event: 'SendSMSClicked',
+//   candidate_id: 'not set',
+//   candidate_code: 'not set',
+//   consultant_id: '<add variable here>',
+//   candidate_status: 'not set',
+//   order_id: 'not set',
+//   staticList_id: 'not set'
+// })
+
+// let (<any>window).dataLayer.allSelectedCandidate || {}
+
+// staticList_id: {
+//   ...datalayer.allSelectedCandidate,
+//   newlist
+// }
